ac31c2761b6c22663e0f042837203c0de05ffce7
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-13 02:16:30 -0400

    add new component for filters

---
+++ rick-and-morty-challenge/src/components/Filters.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex justify-between items-center">
+    <q-input
+      v-model="filter"
+      dense
+      color="green-4"
+      label="Filtrar por personagem"
+    >
+      <template v-slot:append>
+        <q-avatar>
+          <img
+            src="https://i.pinimg.com/originals/98/29/21/9829215db6f9210c0ae4e318e854cb1f.png"
+          />
+        </q-avatar>
+      </template>
+    </q-input>
+    <div class="flex gap-2 items-center">
+      <q-badge
+        :color="getBadgeColor(status)"
+        v-for="(status, index) in statuses"
+        :key="index"
+        class="q-pa-xs cursor-pointer"
+      >
+        {{ status }}
+      </q-badge>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { getBadgeColor } from "../utils/getBadgeColors";
+
+//MODELS
+let filter = ref<string>("");
+let statuses = ref<string[]>(["Alive", "unknown", "Dead"]);
+</script>
