5249eaedb022dda0cffd207a38869aeb64f7e4b9
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-13 01:42:50 -0400

    toggle text colors through dark mode

--- rick-and-morty-challenge/src/components/Pagination.vue
+++ rick-and-morty-challenge/src/components/Pagination.vue
@@ -3,17 +3,24 @@
     v-model="current"
     :max="_infos.pages ?? 0"
     input
-    input-class="text-blue-10"
+    :input-class="$q.dark.isActive ? 'text-green' : 'text-blue'"
+    :color="$q.dark.isActive ? 'green' : 'blue'"
+    icon-first="skip_previous"
+    icon-last="skip_next"
+    icon-prev="fast_rewind"
+    icon-next="fast_forward"
   />
 </template>
 <script setup lang="ts">
 import { ref, watch } from "vue";
 import { useRNMStore } from "../store";
 import { storeToRefs } from "pinia";
+import { useQuasar } from "quasar";
 
 //INSTANCES
 const store = useRNMStore();
 const { _infos } = storeToRefs(store);
+const $q = useQuasar();
 
 //MODELS
 let current = ref<number>(1);
