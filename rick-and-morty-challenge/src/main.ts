5bba732fc5f384cbdf878604fd4dec187b1e25ba
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-12 22:06:35 -0400

    adding pinia

--- rick-and-morty-challenge/src/main.ts
+++ rick-and-morty-challenge/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 
@@ -6,15 +7,16 @@ import { Quasar } from "quasar";
 import quasarLang from "quasar/lang/en-US";
 import quasarIconSet from "quasar/icon-set/material-icons";
 
-// Import Quasar and icon styles
 import "quasar/src/css/index.sass";
 import "@quasar/extras/material-icons/material-icons.css";
 
+const pinia = createPinia();
 const app = createApp(App);
 
 app.use(Quasar, {
-  plugins: {}, // add Quasar plugins here as needed
+  plugins: {},
   lang: quasarLang,
   iconSet: quasarIconSet,
 });
+app.use(pinia);
 app.mount("#app");
