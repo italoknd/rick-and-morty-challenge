d0cdb0c7759b349815262a196ebb97f8dbbc7eaf
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-13 02:58:59 -0400

    importing notify on main ts

--- rick-and-morty-challenge/src/main.ts
+++ rick-and-morty-challenge/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 
-import { Quasar } from "quasar";
+import { Quasar, Notify } from "quasar";
 import quasarLang from "quasar/lang/en-US";
 import quasarIconSet from "quasar/icon-set/material-icons";
 
@@ -14,7 +14,7 @@ const pinia = createPinia();
 const app = createApp(App);
 
 app.use(Quasar, {
-  plugins: {},
+  plugins: { Notify },
   lang: quasarLang,
   iconSet: quasarIconSet,
 });
