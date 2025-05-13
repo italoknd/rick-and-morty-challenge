01265a2883e7cca38da6029dd42d3873ed034bd1
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-12 21:52:11 -0400

    feat: adding quasar on project

--- rick-and-morty-challenge/src/main.ts
+++ rick-and-morty-challenge/src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { Quasar } from "quasar";
+import quasarLang from "quasar/lang/en-US";
+import quasarIconSet from "quasar/icon-set/material-icons";
+
+// Import Quasar and icon styles
+import "quasar/src/css/index.sass";
+import "@quasar/extras/material-icons/material-icons.css";
+
+const app = createApp(App);
+
+app.use(Quasar, {
+  plugins: {}, // add Quasar plugins here as needed
+  lang: quasarLang,
+  iconSet: quasarIconSet,
+});
+app.mount("#app");
