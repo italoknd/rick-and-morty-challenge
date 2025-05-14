8fae1ba3a25083d4592e69f5a056dca4ae5439b9
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-14 17:13:32 -0400

    feat: add router to push back to the main page

--- rick-and-morty-challenge/src/views/CharacterDetails.vue
+++ rick-and-morty-challenge/src/views/CharacterDetails.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-4 flex">
-    <q-card class="my-card" flat bordered>
+  <div class="p-4 flex items-center justify-center">
+    <q-card class="my-card w-[500px] h-[calc(100vh-200px)]" flat bordered>
       <q-img :src="_selectedCharacter.image" />
 
       <q-card-section>
@@ -22,37 +22,41 @@
       </q-card-section>
 
       <q-card-actions>
-        <q-btn flat color="primary" label="See Episodes Participations" />
-        <q-btn flat color="secondary" label="Back to Main Menu" />
+        <q-btn
+          color="green-5"
+          label="Back to Main Menu"
+          @click="router.push('/characters')"
+        />
 
         <q-space />
 
-        <q-btn
-          color="grey"
-          round
-          flat
-          dense
-          :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-          @click="expanded = !expanded"
-        />
+        <q-btn color="grey" round flat dense />
       </q-card-actions>
-
-      <q-slide-transition>
-        <div v-show="expanded">
-          <q-separator />
-          <q-card-section class="text-subtitle2"> </q-card-section>
-        </div>
-      </q-slide-transition>
     </q-card>
+
+    <!--EPISODES LIST-->
+    <q-list bordered class="rounded-borders w-[500px] h-[calc(100vh-200px)]">
+      <q-expansion-item
+        expand-icon-toggle
+        expand-separator
+        label="Episodes"
+        :caption="_selectedCharacter.name"
+      >
+        <q-card>
+          <q-card-section> Lorem ipsum dolor </q-card-section>
+        </q-card>
+      </q-expansion-item>
+    </q-list>
   </div>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRNMStore } from "../store";
 import { storeToRefs } from "pinia";
-
+import { useRouter } from "vue-router";
 //INSTANCES
 const store = useRNMStore();
+const router = useRouter();
 const { _selectedCharacter, _infos } = storeToRefs(store);
 
 //MODELS
