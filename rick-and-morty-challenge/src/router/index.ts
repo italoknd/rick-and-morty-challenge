dbf5ee0c9021f8224515cc35c1f7077cf9bfe431
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-14 21:07:09 -0400

    fix: add hash history to allow page reload

--- rick-and-morty-challenge/src/router/index.ts
+++ rick-and-morty-challenge/src/router/index.ts
@@ -1,15 +1,15 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 
 import Characters from "../views/Characters.vue";
 import CharacterDetails from "../views/CharacterDetails.vue";
 
 const routes = [
   { path: "/", redirect: "/characters" },
-  { path: "/characters", component: Characters },
+  { path: "/characters", component: Characters,  },
   { path: "/character-detail", component: CharacterDetails },
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
